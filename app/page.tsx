65bd9259fbd0a90664c4c5e7675e6b9bed1a473e
Azurieum <mlgopnope@gmail.com>
2024-07-17 17:40:53 -0400

    a

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AudioPlayer from './AudioPlayer';
+
+
+export default function Home() {
+  return (
+  <main className="flex min-h-screen flex-col items-center justify-center bg-red-700">
+  <section className="flex flex-col items-center">
+    <h1 className="text-9xl text-white"></h1>
+    <h2 className="text-9xl text-white"></h2>
+    
+  </section>
+</main>
+  );
+}
